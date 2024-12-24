35ed6e467ccfa44e3b8e59e4460848f9bbac6c2f
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2024-12-24 23:08:48 +0530

    init organizers

---
+++ src/data/Organizers.ts
@@ -0,0 +1,15 @@
+import Omar from "organizers/syedOmarAlbeez.svg";
+
+const Organizers = [
+  {
+    name: "Syed Omar Albeez",
+    image: Omar,
+    role: "Organizer",
+    description: "",
+    github: "https://github.com/amarafray",
+    linkedIn: "https://www.linkedin.com/in/syed-omar-albeez/",
+    type: "organizer",
+  },
+];
+
+export default Organizers;
