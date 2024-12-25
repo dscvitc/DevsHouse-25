c2ae6566745b300b826cee00b9f6d965cc290bb1
Naveen <notnaveen2@gmail.com>
2024-12-25 12:55:01 +0530

    added organizers heading, cube background

--- src/components/OrgBg.tsx
+++ src/components/OrgBg.tsx
@@ -1,9 +1,12 @@
 // this includes the cube and the wavy thingy
+import Image from "next/image";
+import Cube from "../../public/cube.svg";
+import waveVector from "../../public/wavy.svg";
 
 export default function OrgBg() {
   return (
-    <div>
-      <h1>Organizer Background</h1>
+    <div className=" bg-[url('../../public/wavy.svg')] bg-no-repeat bg-contain  bg-bottom flex justify-center">
+      <Image className="" src={Cube} alt="WaveVector background"></Image>
     </div>
   );
 }
--- src/components/OrgHeader.tsx
+++ src/components/OrgHeader.tsx
@@ -1,9 +1,25 @@
 // this containes the heading and the diamond
+import Image from "next/image";
+import Diamond from "../../public/diamond.svg";
+import { Inter, Inter_Tight } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const interTight = Inter_Tight({ subsets: ["latin"] });
 
 export default function OrgHeader() {
   return (
-    <div>
-      <h1>Organizer Header</h1>
+    <div className="relative  h-[210px] sm:h-[300px]  flex justify-center pt-14 sm:pt-[75px] ">
+      <Image
+        src={Diamond}
+        alt="Diamond Vector"
+        layout="fill"
+        objectFit="contain"
+      ></Image>
+      <h1
+        className={`text-5xl sm:text-7xl font-extrabold tracking-tight ${inter.className}`}
+      >
+        ORGANIZERS
+      </h1>
     </div>
   );
 }
