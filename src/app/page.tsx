0b7b968f86202df94d54d5431e0328c1ab0d0cd0
AvikRay1001 <avikray1001@gmail.com>
2024-12-26 21:16:05 +0530

    From Prizes branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ContactUs from '@/components/Contact';
 import Glimpses from '@/components/Glimpses';
 import Tracks from '@/components/tracks/tracks';
 import Timeline from '@/components/Timeline';
-import Prizes from '@/components/prizes/Prizes';
+import Prizes from '@/components/prizes/PrizesComponent';
 
 export default function Home() {
 	return (
