e7c4b42cad302583003afcf63d7b4cc41e296793
Chirag-Jamariya <chiragjamariya01@gmail.com>
2024-12-26 09:00:47 +0530

    Timeline-2

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -3,25 +3,19 @@ import Image from 'next/image';
 const Timeline = () => {
   return (
     <div className="relative flex flex-col items-center  p-6">
-      {/* Heading with Background */}
       <div className="relative flex items-center justify-center w-full mb-8">
-        {/* Background SVG */}
         <div className="absolute inset-0 flex justify-center items-center -z-1 mt-5 mr-14">
           <Image
             src="/Timeline-bg.svg"
             alt="Timeline Background"
-            width={500} // Adjust as needed
-            height={400} // Adjust as needed
+            width={500} 
+            height={400} 
             className="object-contain opacity-40"
             priority
           />
         </div>
-
-        {/* Heading */}
         <h1 className="text-4xl font-bold text-white z-10">TIMELINE</h1>
       </div>
-
-      {/* Timeline SVG */}
       <div className="w-full max-w-4xl">
         <Image
           src="/Timeline.svg"
