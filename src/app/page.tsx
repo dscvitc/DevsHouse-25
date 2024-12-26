bf1e75c6712b32a00f197ee72cfa161d16896a48
aryansharma2k4 <aryansharma2k4@gmail.com>
2024-12-26 15:29:15 +0530

    Added The Host Invitation Component and the Assets required in it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ContactUs from '@/components/Contact';
 import Glimpses from '@/components/Glimpses';
 import Tracks from '@/components/tracks/tracks';
 import Timeline from '@/components/Timeline';
+import HostInvitation from '@/components/HostInvitation';
 
 export default function Home() {
 	return (
@@ -9,7 +10,9 @@ export default function Home() {
 			<Tracks />
 			<Timeline />
 			<Glimpses />
+			<HostInvitation />
 			<ContactUs />
+
 		</>
 	)
 }
