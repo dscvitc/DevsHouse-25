64f6985f7d765203366967400881ee28c2123e5d
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2024-12-26 22:25:31 +0530

    resolved merged conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import ContactUs from "@/components/Contact";
 import Glimpses from "@/components/Glimpses";
 import Tracks from "@/components/tracks/tracks";
 import Timeline from "@/components/Timeline";
+import HostInvitation from "@/components/HostInvitation";
 import About from "@/components/About";
 import Org from "@/components/organizers/Org";
+import Speakers from "@/components/Speakers";
 
 export default function Home() {
   return (
@@ -11,8 +13,9 @@ export default function Home() {
       <About />
       <Tracks />
       <Timeline />
-      <Org />
+      <Speakers />
       <Glimpses />
+      <HostInvitation />
       <ContactUs />
     </>
   );
--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import WireframeBall from './WireframeBall'; 
+import Hyperboloid from './Hyperboloid'; 
+import Dodecahedron from './Dodecahedron'; 
+import Cubedodecahedron from './Cubedodecahedron'; 
 
 interface HeadingProps {
   title: string;
+  geometry: string;
 }
 
-const Heading: React.FC<HeadingProps> = ({ title }) => {
+const Heading: React.FC<HeadingProps> = ({ title, geometry }) => {
+  const renderGeometry = () => {
+    switch (geometry) {
+      case 'Hyperboloid':
+        return <Hyperboloid />;
+      case 'Dodecahedron':
+        return <Dodecahedron />;
+      case 'Cubedodecahedron':
+        return <Cubedodecahedron />;
+      default:
+        return null; // Or a default component if needed
+    }
+  };
+
   return (
     <div className="flex flex-row font-bold relative z-10 mb-8">
-      <div className="flex flex-col justify-end pb-1 md:pb-3 z-10" style={{ marginTop: '40px' }}> {/* Adjust margin as needed */}
+      <div className="flex flex-col justify-end pb-1 md:pb-3 z-10" style={{ marginTop: '40px' }}>
         <p className="text-6xl font-bold mb-8">{title}</p>
       </div>
       <div className="absolute top-5 left-0 w-full h-full flex items-center justify-center z-0">
-        <WireframeBall />
+        {renderGeometry()}
       </div>
     </div>
   );
