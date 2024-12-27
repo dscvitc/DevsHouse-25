a7b504fc7d0f1f8946987a635aabf9b922de920e
Mannat Jaiswal <mannatjaiswal@Mannats-MacBook-Air.local>
2024-12-28 00:33:31 +0530

    Improved navbar mobile view and hero section

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const navbarItems = [
+  {
+    title: "About",
+    href: "#about",
+  },
+  {
+    title: "Themes",
+    href: "#themes",
+  },
+  {
+    title: "Timeline",
+    href: "#timeline",
+  },
+  {
+    title: "Contact Us",
+    href: "#contact-us",
+  },
+];
