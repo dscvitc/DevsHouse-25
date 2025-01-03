ffe5508b3604449c4ef670bb95e0d342602c2ede
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2025-01-03 23:32:55 +0530

    split organizers into co-organizers and tech-team

--- src/components/organizers/OrgGrid.tsx
+++ src/components/organizers/OrgGrid.tsx
@@ -1,49 +1,48 @@
 import OrgCard from "./OrgCard/OrgCard";
 import { Organizers } from "../../data/Organizers";
+import Heading from "../ui/Heading";
 
 export default function OrgGrid() {
   return (
-    <div className="w-full flex justify-center lg:pt-20 md:pt-10">
-      <div className="xl:grid xl:grid-cols-4 hidden w-max gap-8">
-        {Organizers.slice(0, 12).map((organizer) => (
-          <OrgCard
-            key={Math.random() * 100}
-            name={organizer.name}
-            image={organizer.image}
-            linkedIn={organizer.linkedIn}
-          />
-        ))}
+    <>
+      <div className="w-full flex justify-center lg:pt-6 md:pt-2">
+        <div className="grid grid-cols-4 w-max gap-8">
+          {Organizers.slice(0, 4).map((organizer) => (
+            <OrgCard
+              key={Math.random() * 100}
+              name={organizer.name}
+              image={organizer.image}
+              linkedIn={organizer.linkedIn}
+            />
+          ))}
+        </div>
       </div>
-      <div className="lg:grid xl:hidden lg:grid-cols-3 hidden w-max gap-8">
-        {Organizers.slice(0, 6).map((organizer) => (
-          <OrgCard
-            key={Math.random() * 100}
-            name={organizer.name}
-            image={organizer.image}
-            linkedIn={organizer.linkedIn}
-          />
-        ))}
+      <Heading title="CO-ORGANIZERS" geometry=""></Heading>
+      <div className="w-full flex justify-center -mt-8">
+        <div className="grid grid-cols-4  w-max gap-8">
+          {Organizers.slice(4, 8).map((organizer) => (
+            <OrgCard
+              key={Math.random() * 100}
+              name={organizer.name}
+              image={organizer.image}
+              linkedIn={organizer.linkedIn}
+            />
+          ))}
+        </div>
       </div>
-      <div className="md:grid xl:hidden lg:hidden md:grid-cols-2 hidden w-max gap-8">
-        {Organizers.slice(0, 4).map((organizer) => (
-          <OrgCard
-            key={Math.random() * 100}
-            name={organizer.name}
-            image={organizer.image}
-            linkedIn={organizer.linkedIn}
-          />
-        ))}
+      <Heading title="TECHNICAL TEAM" geometry=""></Heading>
+      <div className="w-full flex justify-center -mt-8">
+        <div className="grid grid-cols-4  w-max gap-8">
+          {Organizers.slice(8, 12).map((organizer) => (
+            <OrgCard
+              key={Math.random() * 100}
+              name={organizer.name}
+              image={organizer.image}
+              linkedIn={organizer.linkedIn}
+            />
+          ))}
+        </div>{" "}
       </div>
-      <div className="grid xl:hidden lg:hidden md:hidden grid-cols-1 w-max gap-8">
-        {Organizers.slice(0, 1).map((organizer) => (
-          <OrgCard
-            key={Math.random() * 100}
-            name={organizer.name}
-            image={organizer.image}
-            linkedIn={organizer.linkedIn}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
