5ca00654239b10b819a2decfef987e2474884e32
Raksh-iscool <rakshana3110@gmail.com>
2025-02-03 01:35:13 +0530

    countdown, date changed

--- src/components/hero/CountdownTimer/index.tsx
+++ src/components/hero/CountdownTimer/index.tsx
@@ -1,14 +1,12 @@
 import WordRotate from "@/components/ui/word-rotate";
 import React, { useEffect, useState } from "react";
 
-interface CountdownTimerProps {
-  targetTime: number;
-}
+const CountdownTimer: React.FC = () => {
+  const targetDate = new Date("2025-04-04T00:00:00Z").getTime();
+  const currentDate = new Date().getTime();
+  const initialTimeLeft = Math.floor((targetDate - currentDate) / 1000);
 
-const CountdownTimer: React.FC<CountdownTimerProps> = ({
-  targetTime,
-}) => {
-  const [timeLeft, setTimeLeft] = useState(targetTime);
+  const [timeLeft, setTimeLeft] = useState(initialTimeLeft);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,84 +19,57 @@ const CountdownTimer: React.FC<CountdownTimerProps> = ({
       });
     }, 1000);
 
-    return () => clearInterval(interval); // Cleanup on unmount
+    return () => clearInterval(interval); 
   }, []);
 
-  interface TimeFormat {
-    days: string;
-    hours: string;
-    minutes: string;
-    remainingSeconds: string;
-  }
-
-  const formatTime = (seconds: number): TimeFormat => {
-    const days = Math.floor(seconds / 86400)
-      .toString()
-      .padStart(2, "0"); // 86400 seconds in a day
+  const formatTime = (seconds: number) => {
+    const days = Math.floor(seconds / 86400).toString().padStart(2, "0");
     const hours = Math.floor((seconds % 86400) / 3600)
       .toString()
-      .padStart(2, "0"); // 3600 seconds in an hour
+      .padStart(2, "0");
     const minutes = Math.floor((seconds % 3600) / 60)
       .toString()
-      .padStart(2, "0"); // 60 seconds in a minute
-    const remainingSeconds = (seconds % 60)
-      .toString()
-      .padStart(2, "0"); // Remaining seconds
+      .padStart(2, "0");
+    const remainingSeconds = (seconds % 60).toString().padStart(2, "0");
 
-    return {
-      days,
-      hours,
-      minutes,
-      remainingSeconds,
-    };
+    return { days, hours, minutes, remainingSeconds };
   };
 
-  const { days, hours, minutes, remainingSeconds } =
-    formatTime(timeLeft);
+  const { days, hours, minutes, remainingSeconds } = formatTime(timeLeft);
 
   return (
     <div className="font-bold flex items-center justify-center">
       <div className="flex items-center max-md:gap-x-3 md:gap-x-6 font-outline leading-none">
         <div className="flex items-end justify-center max-md:gap-x-2 md:gap-x-4">
           <WordRotate
-            className="text-[30px] md:text-[122px] font-bold text-white/40 max-md:text-white/80"
-            words={[days.toString()]}
+            className="text-[22px] md:text-[110px] font-bold text-white/40 max-md:text-white/80"
+            words={[days]}
           />
-          <span className="text-[30px] md:text-[122px] text-white/40 max-md:text-white/80">
-            d
-          </span>
+          <span className="text-[22px] md:text-[110px] text-white/40 max-md:text-white/80">d</span>
         </div>
         <span className="md:flex hidden flex-col items-center justify-end gap-y-3">
           <span className="p-2 border-2 border-white/40" />
           <span className="p-2 border-2 border-white/40" />
         </span>
-        <span className="max-md:flex hidden text-white/40 max-md:text-white/80 text-[30px]">
-          :
-        </span>
+        <span className="max-md:flex hidden text-white/40 max-md:text-white/80 text-[22px]">:</span>
         <div className="flex items-end justify-center max-md:gap-x-2 md:gap-x-4">
           <WordRotate
-            className="text-[30px] md:text-[122px] font-bold text-white/40 max-md:text-white/80"
-            words={[hours.toString()]}
+            className="text-[22px] md:text-[110px] font-bold text-white/40 max-md:text-white/80"
+            words={[hours]}
           />
-          <span className="text-[30px] md:text-[122px] text-white/40 max-md:text-white/80">
-            h
-          </span>
+          <span className="text-[22px] md:text-[110px] text-white/40 max-md:text-white/80">h</span>
         </div>
         <span className="md:flex hidden flex-col items-center justify-end gap-y-3">
           <span className="p-2 border-2 border-white/40" />
           <span className="p-2 border-2 border-white/40" />
         </span>
-        <span className="max-md:flex hidden text-white/40 max-md:text-white/80 text-[30px]">
-          :
-        </span>
+        <span className="max-md:flex hidden text-white/40 max-md:text-white/80 text-[22px]">:</span>
         <div className="flex items-end justify-center max-md:gap-x-2 md:gap-x-4">
           <WordRotate
-            className="text-[30px] md:text-[122px] font-bold text-white/40 max-md:text-white/80"
-            words={[minutes.toString()]}
+            className="text-[22px] md:text-[110px] font-bold text-white/40 max-md:text-white/80"
+            words={[minutes]}
           />
-          <span className="text-[30px] md:text-[122px] text-white/40 max-md:text-white/80">
-            m
-          </span>
+          <span className="text-[22px] md:text-[110px] text-white/40 max-md:text-white/80">m</span>
         </div>
       </div>
     </div>
--- src/components/hero/HeroContent/index.tsx
+++ src/components/hero/HeroContent/index.tsx
@@ -8,19 +8,12 @@ import Link from "next/link";
 type Props = {};
 
 const HeroContent = (props: Props) => {
-  //   * Change this time for the countdown timer
   // Target date: April 4, 2025, 00:00:00
-  const targetDate = new Date(
-    "2025-04-04T00:00:00Z"
-  ).getTime();
+  const targetDate = new Date("2025-04-04T00:00:00Z").getTime();
   const currentTime = new Date().getTime();
-  const initialTimeLeft = Math.max(
-    Math.floor((targetDate - currentTime) / 1000),
-    0
-  );
+  const initialTimeLeft = Math.max(Math.floor((targetDate - currentTime) / 1000), 0);
 
-  const [timeLeft, setTimeLeft] =
-    useState<number>(initialTimeLeft);
+  const [timeLeft, setTimeLeft] = useState<number>(initialTimeLeft);
 
   // Update the countdown timer every second
   useEffect(() => {
@@ -37,27 +30,10 @@ const HeroContent = (props: Props) => {
     return () => clearInterval(intervalId); // Cleanup on unmount
   }, []);
 
-  // Format the time into days, hours, minutes, and seconds
-  //   const formatTime = (seconds: number) => {
-  //     const days = Math.floor(seconds / (3600 * 24));
-  //     const hours = Math.floor(
-  //       (seconds % (3600 * 24)) / 3600
-  //     );
-  //     const minutes = Math.floor((seconds % 3600) / 60);
-  //     const remainingSeconds = seconds % 60;
-
-  //     return `${days}d ${hours
-  //       .toString()
-  //       .padStart(2, "0")}h:${minutes
-  //       .toString()
-  //       .padStart(2, "0")}m:${remainingSeconds
-  //       .toString()
-  //       .padStart(2, "0")}s`;
-  //   };
   return (
-    <div className="flex flex-col justify-center items-center gap-y-0 w-full">
+    <div className="flex flex-col justify-center items-center gap-y-0 w-full mt-4">
       <div className="flex flex-col justify-center items-start gap-y-0">
-        <div className=" flex flex-col justify-center items-start text-white w-full md:max-w-[875px] ">
+        <div className="flex flex-col justify-center items-start text-white w-full md:max-w-[875px]">
           <div className="flex flex-col justify-center max-md:items-center md:items-start">
             <div className="flex justify-center items-start">
               <div className="flex flex-col justify-center items-start">
@@ -65,7 +41,7 @@ const HeroContent = (props: Props) => {
                   DEVSHOUSE'
                 </span>
                 <span className="flex max-md:flex-col justify-start gap-x-8 items-center w-full m-0 p-0 leading-none md:translate-x-4">
-                  <div className="max-md:grid max-md:grid-cols-2 max-md:gap-x-5 flex justify-start gap-x-8 items-center ">
+                  <div className="max-md:grid max-md:grid-cols-2 max-md:gap-x-5 flex justify-start gap-x-8 items-center">
                     <span className="col-span-1 font-mona-sans-bold font-extrabold text-[32px] max-md:text-[28px]">
                       HACK IT.
                     </span>
@@ -73,7 +49,7 @@ const HeroContent = (props: Props) => {
                       BUILD IT.
                     </span>
                   </div>
-                  <span className="font-mona-sans-bold  font-extrabold text-[32px]">
+                  <span className="font-mona-sans-bold font-extrabold text-[32px]">
                     OWN IT.
                   </span>
                 </span>
@@ -84,44 +60,35 @@ const HeroContent = (props: Props) => {
             </div>
             <div className="max-md:text-base md:text-[22px] text-[#AEAEAE] text-left font-mona-sans max-md:mt-3 px-4">
               DevsHouse '25 is a{" "}
-              <span className="text-[#5B8EFF]">
-                national-level
-              </span>{" "}
-              hackathon where students from colleges across
-              the country will have{" "}
-              <span className="text-[#FF8171]">
-                36 hours
-              </span>{" "}
-              to develop real-world solutions using a range
-              of cutting-edge tech stacks.
+              <span className="text-[#5B8EFF]">national-level</span> hackathon where students from colleges across the country will have{" "}
+              <span className="text-[#FF8171]">36 hours</span> to develop real-world solutions using a range of cutting-edge tech stacks.
             </div>
+
+            <div className="max-md:text-base md:text-[26px] text-[#AEAEAE] text-center font-mona-sans mt-2 px-4">
+              <a
+                href="https://www.google.com/calendar/render?action=TEMPLATE&text=Devshouse+25&dates=20250404T000000Z/20250406T235959Z&details=Event+Details&location=Event+Location"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="text-[#AEAEAE]"
+              >
+                <span className="text-[#5B8EFF]">Mark your calendar:</span>{" "}
+                <span className="font-bold text-[#FF8171]">4th to 6th April</span>
+              </a>
+            </div>
+
           </div>
         </div>
         <div className="px-4">
           <CountdownTimer targetTime={timeLeft} />
         </div>
         <div className="flex md:gap-x-4 max-md:justify-center max-md:gap-x-4 max-md:px-2 w-full">
-          <Link
-            target="_blank"
-            href="https://devshouse25.devfolio.co/"
-            passHref
-          >
-            <Button
-              size="compact-lg"
-              className="button-Sponsor mt-3 md:mx-4"
-            >
+          <Link target="_blank" href="https://devshouse25.devfolio.co/" passHref>
+            <Button size="compact-lg" className="button-Sponsor mt-3 md:mx-4">
               Register Now!
             </Button>
           </Link>
-          <Link
-            target="_blank"
-            href="https://discord.com/invite/UFfPjTtzh7"
-            passHref
-          >
-            <Button
-              size="compact-lg"
-              className="button-Sponsor mt-3 md:mx-4"
-            >
+          <Link target="_blank" href="https://discord.com/invite/UFfPjTtzh7" passHref>
+            <Button size="compact-lg" className="button-Sponsor mt-3 md:mx-4">
               Community Page
             </Button>
           </Link>
