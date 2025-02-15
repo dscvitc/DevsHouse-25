74b948d9a84067fd4c03927740d4c83ac9e7bcac
Ayush Awasthi <ayush.awasthi468@gmail.com>
2025-02-15 15:26:45 +0530

    Improve load time

--- src/components/hero/HeroBG/index.tsx
+++ src/components/hero/HeroBG/index.tsx
@@ -7,15 +7,18 @@ type Props = {
 
 const HeroBG = ({ children }: Props) => {
   return (
-    <div className=" flex flex-col justify-between w-full h-full">
+    <div className="flex flex-col justify-between w-full h-full">
       <div className="flex flex-col justify-start relative inset-0 w-full overflow-hidden h-full">
-
-        <div className="flex justify-between ">
+        {/* First Row of Images */}
+        <div className="flex justify-between">
           <Image
             src="/images/g91-2.svg"
             alt="bg-asset"
             height={400}
             width={280}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative -top-20 max-md:-left-10 max-md:scale-75"
           />
           <Image
@@ -23,6 +26,9 @@ const HeroBG = ({ children }: Props) => {
             alt="cone"
             height={100}
             width={200}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative left-7 -top-[10px] h-[200px]"
           />
           <Image
@@ -30,6 +36,9 @@ const HeroBG = ({ children }: Props) => {
             alt="bg-asset-cube"
             height={488}
             width={260}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative -left-48 -top-16 max-md:-top-20 max-md:hidden"
           />
           <Image
@@ -37,6 +46,9 @@ const HeroBG = ({ children }: Props) => {
             alt="bg-asset"
             height={580}
             width={600}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative -left-[292px] -top-[66px]"
           />
           <Image
@@ -44,6 +56,9 @@ const HeroBG = ({ children }: Props) => {
             alt="bg-asset"
             height={420}
             width={380}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative -left-[443px] -top-[70px]"
           />
           <Image
@@ -51,16 +66,23 @@ const HeroBG = ({ children }: Props) => {
             alt="bg-asset"
             height={420}
             width={380}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="z-[1000] relative -top-12 -left-[490px] right-0 max-md:left-auto max-md:right-0"
           />
         </div>
 
+        {/* Second Row of Images */}
         <div className="flex justify-between w-full">
           <Image
             src="/images/wireframe-50.svg"
             alt="bg-asset"
             height={250}
             width={250}
+            priority
+            placeholder="empty"
+            loading="eager"
             className="-z-10 relative -top-[100px] opacity-50 max-md:-left-28 max-md:opacity-30"
           />
 
@@ -69,10 +91,13 @@ const HeroBG = ({ children }: Props) => {
             alt="bg-asset"
             height={402}
             width={424}
-            className="-z-50 relative -right-50 -top-28 "
+            priority
+            placeholder="empty"
+            loading="eager"
+            className="-z-50 relative -right-50 -top-28"
           />
-          <div className="-z-50  absolute right-0 top-[260px] w-[424px] h-[544px] bg-gradient-to-r from-transparent via-[#131313] to-[#131313] opacity-50 pointer-events-none"></div>
 
+          <div className="-z-50 absolute right-0 top-[260px] w-[424px] h-[544px] bg-gradient-to-r from-transparent via-[#131313] to-[#131313] opacity-50 pointer-events-none"></div>
         </div>
       </div>
       <div className="absolute inset-0">{children}</div>
@@ -80,4 +105,4 @@ const HeroBG = ({ children }: Props) => {
   );
 };
 
-export default HeroBG; 
+export default HeroBG;
