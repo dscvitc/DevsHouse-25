481a6e3e304ea48802b5193f7906df215b88f7f8
ayush.awasrhi2022 <ayush.awasrhi2022@vitstudent.ac.in>
2025-03-25 03:18:25 +0000

    Add mlh special track prizes

--- src/components/prizes/PrizesComponent.tsx
+++ src/components/prizes/PrizesComponent.tsx
@@ -77,15 +77,33 @@ export default function Prizes() {
       imageWidth: 250,
         imageHeight: 80,
     },
-/* {
-  trackName: "Company F",
-    trackLogo: "/images/icons-track-prize.png",
+ {
+  trackName: "Gemini",
+    trackLogo: "/sponsors/genai-prize-category.webp",
       trackText:
-  "Top open-source projects fostering transparent and collaborative tech development win INR 10,000.",
+  "Best Use of Gemini API: Assorted Prizes - Push the limits of AI with the Google Gemini API! Build apps that understand language, analyze data, and generate creative content.",
     color: "#07A158",
-      imageWidth: 160,
+      imageWidth: 200,
         imageHeight: 80,
-    }, */
+    }, 
+    {
+      trackName: "Midnight",
+        trackLogo: "/sponsors/midnight.webp",
+          trackText:
+      "Best Use of Midnight - JBL Tune 510BT Wireless Headphones: Leverage Midnight's blockchain to build secure, data-protected DApps and win wireless headphones for your entire team!",
+        color: "#07A158",
+          imageWidth: 200,
+            imageHeight: 80,
+        }, 
+        {
+          trackName: "MongoDB",
+            trackLogo: "/sponsors/mongodb.webp",
+              trackText:
+          "Best Use of MongoDB Atlas - M5GO IoT Starter Kit: Build a hack using MongoDB Atlas for a chance to win a M5GO IoT Starter Kit for you and each member of your team!",
+            color: "#07A158",
+              imageWidth: 200,
+                imageHeight: 80,
+            }, 
   ];
 
 
